3c0974d28881fb76dd0ca01f44dff7f2eb034823
Sira Hanchana <sira.hanchana@gmail.com>
2015-04-23 17:32:43 +0700

    update-responsiveness
    update-responsiveness

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,30 @@
 /* jshint devel:true */
 
-$(".dropdown-menu li a").click(function(){
+$('.language > li > a').click(function(){
+	var get_lang_flag = $(this).find('span:eq(0)').attr('class');
+	var get_lang_text = $(this).find('span:eq(1)').text();
+	var temp_init_flag = $('.language-initial').find('span:eq(0)').attr('class');
+	var temp_init_text = $('.language-initial').find('.extra-text').text();
+
+	
+	$(this).find('span:eq(0)').removeClass();
+	$(this).find('span:eq(0)').addClass(temp_init_flag);
+	$(this).find('span:eq(1)').html(temp_init_text);
+
+	$('.language-initial').find('span:eq(0)').removeClass();
+	$('.language-initial').find('span:eq(0)').addClass(get_lang_flag);
+	$('.language-initial').find('.extra-text').html(get_lang_text);
+
+
+
+
+	$('.language-initial').attr('aria-expanded', false);
+	$('.language-wrapper').removeClass('open');
+
+});
+
+
+$(".dropdown-menu:not(.language) li a").click(function(){
   var selectedText = $(this).text();
   var storeIntText = $(this).parents('.btn-group').find('.btn-int > .btn-text').text();
 
@@ -15,11 +39,37 @@ $(".dropdown-menu li a").click(function(){
 
 });
 
-$(".country-group > button, .area-group > button").click(function(){
-
-	
+/* ---- Start Autocomplete 'Typeahead' --> For sample go to (http://twitter.github.io/typeahead.js/examples/) ---- */
+var countries = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  limit: 10,
+  prefetch: {
+    // url points to a json file that contains an array of country names, see
+    url: '/scripts/states.json',
+    // the json file contains an array of strings, but the Bloodhound
+    // suggestion engine expects JavaScript objects so this converts all of
+    // those strings
+    filter: function(list) {
+      return $.map(list, function(country) { return { name: country }; });
+    }
+  }
 });
 
+// kicks off the loading/processing of `local` and `prefetch`
+countries.initialize();
+
+$('.typeahead').typeahead({
+	hint:true,
+	highlight:true,
+	minLength: 1
+},
+{
+  name: 'countries',
+  displayKey: 'name',
+  source: countries.ttAdapter()
+});
+/* ---- END Autocomplete 'Typeahead' ----- */
 
 function cloneToTextArea(){
 	var countryText = $(".country-group").find('.selected').text();
@@ -55,12 +105,10 @@ $('.extra p').click(function(){
 		$('.extra-symbol').html('+');
 	}
 	$('.form-bottom').slideDown('fast');
-	$('.check-out-button').hide();
 });
 
 $('.btn-int, .check-in, .check-out').click(function(){
 	$('.form-bottom').slideDown('fast');
-	$('.check-out-button').hide();
 });
 
 // $(document).click(function(e) {
--- dist/scripts/main.js
+++
@@ -1,1 +0,0 @@
-$(".dropdown-menu li a").click(function(){var t=$(this).text(),n=$(this).parents(".btn-group").find(".btn-int").text();$(this).html(n),$(this).parents(".btn-group").find(".dropdown-toggle").html(t+' <span class="caret"></span>')});
